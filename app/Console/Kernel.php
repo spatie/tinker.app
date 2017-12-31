d3f336931bd517bd074e16c0a4f063a7990ce1b7
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-12-31 19:36:00 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\DockerStartContainer;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        DockerStartContainer::class,
     ];
 
     /**
