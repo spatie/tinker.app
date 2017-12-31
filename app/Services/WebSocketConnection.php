34f5404788aec44271838a708400fdc0fef02bb5
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-12-31 19:34:42 +0100

    wip

--- app/Services/WebSocketConnection.php
+++ app/Services/WebSocketConnection.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use \resource;
 use Evenement\EventEmitter;
 use React\EventLoop\LoopInterface;
 use React\Socket\ConnectionInterface;
@@ -12,11 +13,13 @@ use React\Stream\WritableStreamInterface;
 
 class WebSocketConnection extends EventEmitter implements ConnectionInterface
 {
+    /** @var \resource */
     public $stream;
 
+    /** @var \React\Stream\DuplexResourceStream */
     private $input;
 
-    public function __construct($resource, LoopInterface $loop)
+    public function __construct(resource $resource, LoopInterface $loop)
     {
         $this->input = new DuplexResourceStream(
             $resource,
@@ -106,29 +109,10 @@ class WebSocketConnection extends EventEmitter implements ConnectionInterface
             return null;
         }
 
-        if (true) {
-            // remove trailing colon from address for HHVM < 3.19: https://3v4l.org/5C1lo
-            // note that technically ":" is a valid address, so keep this in place otherwise
-            if (substr($address, -1) === ':' && defined('HHVM_VERSION_ID') && HHVM_VERSION_ID < 31900) {
-                $address = (string)substr($address, 0, -1);
-            }
-
-            // work around unknown addresses should return null value: https://3v4l.org/5C1lo and https://bugs.php.net/bug.php?id=74556
-            // PHP uses "\0" string and HHVM uses empty string (colon removed above)
-            if ($address === '' || $address[0] === "\x00") {
-                return null;
-            }
-
-            return 'unix://' . $address;
+        if ($address === '' || $address[0] === "\x00") {
+            return null;
         }
 
-        // check if this is an IPv6 address which includes multiple colons but no square brackets
-        $pos = strrpos($address, ':');
-        if ($pos !== false && strpos($address, ':') < $pos && substr($address, 0, 1) !== '[') {
-            $port = substr($address, $pos + 1);
-            $address = '[' . substr($address, 0, $pos) . ']:' . $port;
-        }
-
-        return (true ? 'tls' : 'tcp') . '://' . $address;
+        return 'unix://' . $address;
     }
 }
