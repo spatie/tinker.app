19c6712b81150afdb3b06946ecef8e5c9a2e1dd9
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-12-31 16:38:43 +0100

    ejo

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,7 +10,6 @@
       <script>
       	var term = new Terminal();
         term.open(document.getElementById('#terminal'));
-        term.write('Hello from \033[1;3;31mxterm.js\033[0m $ ')
 
 
         var socket = new WebSocket('ws://{{ config('websockets.host') }}:{{ config('websockets.port') }}/');
