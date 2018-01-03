d87be1f13a6ebc7a569fa180790dd463cd9b8acb
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-01-03 16:46:41 +0100

    disable debug comment

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,8 +8,8 @@ Terminal.loadAddon('attach');
 const xterm = new Terminal();
 xterm.open(document.getElementById('terminal'), true);
 
-// const socket = new WebSocket(`ws://${window.webSocket.host}:${window.webSocket.port}/`);
-const socket = new WebSocket(`ws://165.227.172.206:8080/`);
+const socket = new WebSocket(`ws://${window.webSocket.host}:${window.webSocket.port}/`);
+// const socket = new WebSocket(`ws://165.227.172.206:8080/`); // ez debug
 
 xterm.attach(socket);
 xterm.fit();
