65ac0f9ab16c6f474db728df178547f2ae59ac89
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-10-16 00:03:17 +0200

    wip ws

--- resources/assets/js/WebSocketConnection.js
+++ resources/assets/js/WebSocketConnection.js
@@ -24,7 +24,7 @@ export const WebSocketConnection = new Vue({
         async openWebSocket() {
             await this.$store.dispatch('fetchSession');
 
-            this.webSocket = new WebSocket(`${window.webSocket.protocol}://${window.webSocket.host}:${window.webSocket.port}/${this.sessionId}`);
+            this.webSocket = new WebSocket(`${window.webSocket.protocol}://localhost/ws/${this.sessionId}`);
 
             this.webSocket.onmessage = (message) => {
                 const data = JSON.parse(message.data);
