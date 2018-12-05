6f62376ba91dfb1561f10cd3a84e110c1a5b7c09
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-05 17:09:31 +0100

    Dont select random code

--- resources/assets/js/editor/FileEditor.vue
+++ resources/assets/js/editor/FileEditor.vue
@@ -2,50 +2,50 @@
     <div id="editor"></div>
 </template>
 <script>
-import * as ace from 'brace';
-import 'brace/mode/php';
-import 'brace/theme/github';
-import 'brace/theme/monokai';
-import {
-    WebSocketConnection
-} from '../WebSocketConnection';
+    import * as ace from 'brace';
+    import 'brace/mode/php';
+    import 'brace/theme/github';
+    import 'brace/theme/monokai';
+    import {
+        WebSocketConnection,
+    } from '../WebSocketConnection';
 
-export default {
-    data() {
-        return {
-            editor: null,
-            lastDelta: null,
-        };
-    },
-
-    computed: {
-        darkMode() {
-            return this.$store.state.darkMode;
+    export default {
+        data() {
+            return {
+                editor: null,
+                lastDelta: null,
+            };
         },
-    },
 
-    watch: {
-        darkMode(darkMode) {
-            this.editor.setTheme((darkMode ? 'ace/theme/monokai' : 'ace/theme/github'));
-        }
-    },
+        computed: {
+            darkMode() {
+                return this.$store.state.darkMode;
+            },
+        },
 
-    created() {
-        WebSocketConnection.$on('message-received', this.onWebSocketMessage);
-    },
+        watch: {
+            darkMode(darkMode) {
+                this.editor.setTheme((darkMode ? 'ace/theme/monokai' : 'ace/theme/github'));
+            },
+        },
 
-    mounted() {
-        this.editor = ace.edit('editor');
+        created() {
+            WebSocketConnection.$on('message-received', this.onWebSocketMessage);
+        },
 
-        this.editor.getSession().setMode({
-            path: "ace/mode/php",
-            inline: true
-        });
+        mounted() {
+            this.editor = ace.edit('editor');
 
-        this.editor.setTheme('ace/theme/github');
+            this.editor.getSession().setMode({
+                path: 'ace/mode/php',
+                inline: true,
+            });
 
-        this.editor.setValue(
-`//use cmd+s or ctrl+s to save and run.
+            this.editor.setTheme('ace/theme/github');
+
+            this.editor.setValue(
+                `//use cmd+s or ctrl+s to save and run.
 
 $n = 100;
 
@@ -65,40 +65,36 @@ for ($i = 1; $i <= $n; $i++) {  //numbers to be checked as prime
         print $i." is Prime\\n";
     }
 }
-`
-        );
+`, 1);
 
-        this.editor.on('change', delta => {
-            if (this.lastDelta != delta) {
-                console.log(JSON.stringify(delta));
-                WebSocketConnection.send('buffer-change', delta);
-            }
-        });
+            this.editor.on('change', delta => {
+                if (this.lastDelta != delta) {
+                    console.log(JSON.stringify(delta));
+                    WebSocketConnection.send('buffer-change', delta);
+                }
+            });
 
-        this.editor.commands.addCommand({
-            name: 'save',
-            bindKey: {
-                win: 'Ctrl-S',
-                mac: 'Command-S'
+            this.editor.commands.addCommand({
+                name: 'save',
+                bindKey: {
+                    win: 'Ctrl-S',
+                    mac: 'Command-S',
+                },
+                exec: this.saveFile,
+                readOnly: true,
+            });
+        },
+
+        methods: {
+            saveFile(editor) {
+                WebSocketConnection.send('buffer-run', editor.getValue());
             },
-            exec: this.saveFile,
-            readOnly: true // false if this command should not apply in readOnly mode
-        });
-    },
-
-    methods: {
-        saveFile(editor) {
-            WebSocketConnection.send('buffer-run', editor.getValue());
+            onWebSocketMessage({ type, payload }) {
+                if (type === 'buffer-change') {
+                    this.lastDelta = JSON.parse(payload);
+                    this.editor.getSession().getDocument().applyDeltas([this.lastDelta]);
+                }
+            },
         },
-        onWebSocketMessage({
-            type,
-            payload
-        }) {
-            if (type === 'buffer-change') {
-                this.lastDelta = JSON.parse(payload);
-                this.editor.getSession().getDocument().applyDeltas([this.lastDelta]);
-            }
-        },
-    }
-}
+    };
 </script>
