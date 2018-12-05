691e2d4744f87b55293236fb1ae656886f4fee9c
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-05 17:17:56 +0100

    No more tinker session routes

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('session', 'TinkerSessionsController@start');
-Route::get('session/{sessionId}', 'TinkerSessionsController@load');
