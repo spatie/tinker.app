f291e7dd5f0c0a9e74ce3d56c93f34de42fbc1dc
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-01-24 22:36:54 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
-        $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
     }
 }
