423b365ef47218364b379e9c249ee57d59d00857
Willem Van Bockstal <willem@spatie.be>
2019-01-31 10:53:10 +0100

    Terminal light color scheme

--- resources/js/terminal/themes/light.js
+++ resources/js/terminal/themes/light.js
@@ -1,4 +1,29 @@
 export default {
     background: '#ffffff',
     foreground: '#000000',
+    fadeColor: '#a09f91',
+    /* black dark/light */
+    color0: '#000000',
+    color8: '#828282',
+    /* red dark/light */
+    color1: '#D14',
+    color9: '#efc6d0',
+    /* green dark/light */
+    color2: '#009926',
+    color10: '#a7dbb4',
+    /* yellow dark/light */
+    color3: '#d2ce33',
+    color11: '#fffed5',
+    /* blue dark/light */
+    color4: '#0086B3',
+    color12: '#a7cfdc',
+    /* magenta dark/light */
+    color5: '#e020a3',
+    color13: '#e6bdd9',
+    /* cyan dark/light */
+    color6: '#099',
+    color14: '#abe8e8',
+    /* white dark/light */
+    color7: '#ababab',
+    color15: '#fff',
 };
