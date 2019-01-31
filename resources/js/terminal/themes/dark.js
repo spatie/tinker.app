ddaa6ba76eae83f25d5a219f930f5407a9f459f7
Willem Van Bockstal <willem@spatie.be>
2019-01-31 13:45:42 +0100

    Fix terminal styles

--- resources/js/terminal/themes/dark.js
+++ resources/js/terminal/themes/dark.js
@@ -1,13 +1,11 @@
 export default {
     background: '#272822',
-    foreground: '#F8F8F2',
-    cursor: '#8b6cc4',
-    cursorAccent: '#8b6cc4',
-    selection: 'rgba(150,120,200,.3)',
     blue: '#a09f91', /* cfr. comment color from editor */
-    black: '#ffffff',
-    white: '#000000',
-    yellow: '#fffed5',
-    green: '#a7dbb4',
-    red: '#efc6d0',
+    cursor: '#706b5c',
+    cursorAccent: '#8b6cc4',
+    foreground: '#F8F8F2',
+    green: 'rgba(0, 53, 38, .5)',
+    red: 'rgba(221, 17, 86, .5)',
+    selection: 'rgba(150, 120, 200, .3)',
+    yellow: 'rgba(210, 182, 52, .5)',
 };
--- resources/js/terminal/themes/light.js
+++ resources/js/terminal/themes/light.js
@@ -1,13 +1,11 @@
 export default {
     background: '#ffffff',
-    foreground: '#000000',
-    cursor: '#8b6cc4',
-    cursorAccent: '#8b6cc4',
-    selection: 'rgba(150,120,200,.3)',
     blue: '#a09f91', /* cfr. comment color from editor */
-    black: '#000000',
-    white: '#ffffff',
-    yellow: '#fffed5',
-    green: '#a7dbb4',
-    red: '#efc6d0',
+    cursor: '#706b5c',
+    cursorAccent: '#8b6cc4',
+    foreground: '#000000',
+    green: 'rgba(0, 53, 38, .5)',
+    red: 'rgba(221, 17, 86, .5)',
+    selection: 'rgba(150, 120, 200, .3)',
+    yellow: 'rgba(210, 182, 52, .5)',
 };
