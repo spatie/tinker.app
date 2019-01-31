5777f94d068d932adf22d9575f47f12068f1d7c3
Willem Van Bockstal <willem@spatie.be>
2019-01-31 11:07:06 +0100

    Terminal light color scheme

--- resources/js/terminal/themes/dark.js
+++ resources/js/terminal/themes/dark.js
@@ -1,4 +1,29 @@
 export default {
     background: '#272822',
     foreground: '#F8F8F2',
+    fadeColor: '#a09f91',
+    /* black light/dark */
+    color0: '#828282',
+    color8: '#000000',
+    /* red light/dark */
+    color1: '#efc6d0',
+    color9: '#D14',
+    /* green light/dark */
+    color2: '#a7dbb4',
+    color10: '#009926',
+    /* yellow light/dark */
+    color3: '#fffed5',
+    color11: '#d2ce33',
+    /* blue light/dark */
+    color4: '#a7cfdc',
+    color12: '#0086B3',
+    /* magenta light/dark */
+    color5: '#e6bdd9',
+    color13: '#e020a3',
+    /* cyan light/dark */
+    color6: '#abe8e8',
+    color14: '#099',
+    /* white light/dark */
+    color7: '#fff',
+    color15: '#ababab',
 };
--- resources/js/terminal/themes/light.js
+++ resources/js/terminal/themes/light.js
@@ -1,29 +1,23 @@
 export default {
     background: '#ffffff',
     foreground: '#000000',
-    fadeColor: '#a09f91',
-    /* black dark/light */
-    color0: '#000000',
-    color8: '#828282',
-    /* red dark/light */
-    color1: '#D14',
-    color9: '#efc6d0',
-    /* green dark/light */
-    color2: '#009926',
-    color10: '#a7dbb4',
-    /* yellow dark/light */
-    color3: '#d2ce33',
-    color11: '#fffed5',
-    /* blue dark/light */
-    color4: '#0086B3',
-    color12: '#a7cfdc',
-    /* magenta dark/light */
-    color5: '#e020a3',
-    color13: '#e6bdd9',
-    /* cyan dark/light */
-    color6: '#099',
-    color14: '#abe8e8',
-    /* white dark/light */
-    color7: '#ababab',
-    color15: '#fff',
+    black: '#000000',
+    blue: '#0086B3',
+    brightBlack: '#828282',
+    brightBlue: '#a7cfdc',
+    brightCyan: '#abe8e8',
+    brightGreen: '#a7dbb4',
+    brightMagenta: '#e6bdd9',
+    brightRed: '#efc6d0',
+    brightWhite: '#ababab',
+    brightYellow: '#fffed5',
+    cursor: '#a09f91',
+    cursorAccent: '#a09f91',
+    cyan: '#099',
+    green: '#009926',
+    magenta: '#e020a3',
+    red: '#d14',
+    selection: '#a09f91',
+    white: '#fff',
+    yellow: '##d2ce33',
 };
